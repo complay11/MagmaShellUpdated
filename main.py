7f4e6f0b729fa6c62618389040478e3e4a612af0
Wojtmic <84672664+complay11@users.noreply.github.com>
2022-11-24 08:56:57 +0100

    Writed some code but me too lazy to do moar now.

---
+++ main.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+def loadPackages():
+    pass
+
+print("=====MagmaShell=====")
+print("SYSTEM INFORMATION:")
+print(f"CPU Cores: {multiprocessing.cpu_count()}")
+print("====================")
+
