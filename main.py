819f6c81a0cb51b00de97c9603cdfc5e1088be80
Wojtmic <84672664+complay11@users.noreply.github.com>
2022-11-24 17:58:38 +0100

    moar thingis baby!!!!111!!!!

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import multiprocessing
+import os
 
 def loadPackages():
-    pass
+    rootdir = 'packages'
+    for file in os.listdir(rootdir):
+        d = os.path.join(rootdir, file)
+        if os.path.isdir(d):
+            d = d.replace("packages\\", "")
+            print(d)
 
 print("=====MagmaShell=====")
 print("SYSTEM INFORMATION:")
 print(f"CPU Cores: {multiprocessing.cpu_count()}")
 print("====================")
 
+loadPackages()
