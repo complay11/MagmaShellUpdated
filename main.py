29fa7c1dca064dd3be87499ed976419969c1e474
Wojtmic <84672664+complay11@users.noreply.github.com>
2022-11-25 16:46:11 +0100

    moarrrrrrrrrrr

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import multiprocessing
 import os
+import json
 
 def loadPackages():
     rootdir = 'packages'
     for file in os.listdir(rootdir):
         d = os.path.join(rootdir, file)
         if os.path.isdir(d):
-            d = d.replace("packages\\", "")
-            print(d)
+            if os.path.isfile(f"{d}\\manifest.json"):
+                with open(f"{d}\\manifest.json") as manifest:
+                    load = json.load(manifest)
+                    pckname = load["manifest"]["name"]
+                    print(f"Loading package {pckname}.")
+            else:
+                print(f"Package \"{d}\" is invalid.")
 
 print("=====MagmaShell=====")
 print("SYSTEM INFORMATION:")
